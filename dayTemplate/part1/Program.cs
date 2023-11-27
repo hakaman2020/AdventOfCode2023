c2a2af2831e6f6fedb084d01539be249cbc7749d
hman <hokai.man@centric.eu>
2023-11-27 11:36:52 +0100

    Initial Template done

---
+++ dayTemplate/part1/Program.cs
@@ -0,0 +1,11 @@
+﻿using AoC.Input;
+
+const string inputFile = "./example.txt";
+// const string inputFile = "./input.txt";
+
+var lines = Input.ReturnLinesFromInput(inputFile);
+
+foreach(var line in lines){
+    Console.WriteLine(line);
+}
+
