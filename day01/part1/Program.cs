9a585c6fe067e919db4ccaceb3c19ff6c4c0b688
hman <hman@student.codam.nl>
2023-11-30 22:29:30 +0100

    prep day01

---
+++ day01/part1/Program.cs
@@ -0,0 +1,21 @@
+﻿using AoC.Input;
+
+const string inputFile = "./example.txt";
+// const string inputFile = "./input.txt";
+
+var lines = Input.ReturnLinesFromInput(inputFile);
+
+foreach(var line in lines){
+    Console.WriteLine(line);
+}
+
+Console.WriteLine($"Task1 : {Task1()}");
+Console.WriteLine($"Task2 : {Task2()}");
+
+int Task1(){
+    return -1;
+}
+
+int Task2(){
+    return -1;
+}
