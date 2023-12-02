6554e46d1adb196435960ebcc6f0321111752682
hman <hman@student.codam.nl>
2023-12-02 07:03:22 +0100

    day02

--- day02/part1/Program.cs
+++ day02/part1/Program.cs
@@ -1,21 +1,95 @@
-﻿using AoC.Input;
+﻿using System.Diagnostics.CodeAnalysis;
+using AoC.Input;
 
-const string inputFile = "./example.txt";
-// const string inputFile = "./input.txt";
+const int RedCubes = 12;
+const int BlueCubes = 14;
+const int GreenCubes = 13;
+//  const string inputFile = "./example.txt";
+const string inputFile = "./input.txt";
 
 var lines = Input.ReturnLinesFromInput(inputFile);
-
-foreach(var line in lines){
-    Console.WriteLine(line);
-}
 
 Console.WriteLine($"Task1 : {Task1(lines)}");
 Console.WriteLine($"Task2 : {Task2(lines)}");
 
 int Task1(IEnumerable<string> lines){
-    return -1;
+    int sum = 0;
+
+    foreach(var line in lines){
+        
+        var split = line.Split(':');
+        int gamenumber = int.Parse(split[0].Split(' ')[1]);
+        
+        bool impossibleCombo = false;
+        var combos = split[1].Split(';');
+        foreach(var combo in combos){
+            if(!PossibleCombo(combo)){
+                impossibleCombo = true;
+                break;
+            }
+        }
+        if(!impossibleCombo){
+            sum+= gamenumber;
+        }
+        
+    }
+    return sum;
 }
 
 int Task2(IEnumerable<string> lines){
-    return -1;
+   int sum = 0;
+
+    foreach(var line in lines){
+        
+        var split = line.Split(':');
+        
+        sum += CalcPower(split[1]);
+    }
+    return sum;
+}
+
+
+Boolean PossibleCombo(string combo)
+{
+    int[] BlueGreenRed = [0,0,0];
+    var split = combo.Split(new char[]{' ',','}, StringSplitOptions.RemoveEmptyEntries);
+    for(int i = 0; i < split.Length; i += 2){
+        string currentColor = split[i + 1];
+        int amount = int.Parse(split[i]);
+        if(currentColor.Equals("blue")){
+            if(amount > BlueCubes)
+                return false;
+        } else if (currentColor.Equals("green")){
+            if(amount > GreenCubes)
+                return false;
+        } else if (currentColor.Equals("red")){
+            if(amount > RedCubes)
+                return false;
+        }
+    }
+    return true;
+}
+
+int CalcPower(string game){
+    int[] MinBlueGreenRed = [0, 0, 0];
+    var combos = game.Split(';');
+    foreach(var combo in combos){
+        var split = combo.Split(new char[]{' ',','}, StringSplitOptions.RemoveEmptyEntries);
+        for(int i = 0; i < split.Length; i += 2){
+            string currentColor = split[i + 1];
+            int amount = int.Parse(split[i]);
+            if(currentColor.Equals("blue")){
+                MinBlueGreenRed[0] = amount > MinBlueGreenRed[0] ? amount : MinBlueGreenRed[0];
+            } else if (currentColor.Equals("green")){
+                MinBlueGreenRed[1] = amount > MinBlueGreenRed[1] ? amount : MinBlueGreenRed[1];
+            } else if (currentColor.Equals("red")){
+                MinBlueGreenRed[2] = amount > MinBlueGreenRed[2] ? amount : MinBlueGreenRed[2];
+            }
+        }
+   }
+   int sum = 1;
+   foreach(int amount in MinBlueGreenRed){
+    sum *=amount;
+   }
+   return sum;
 }
